bed2c75b1547ffbe787cb8624b62468d5c6577aa
Josh Thomas <josh@ionic.io>
2016-06-29 13:41:27 -0500

    Updated offline compile script.

--- offline_compile.sh
+++ offline_compile.sh
@@ -1,4 +1,12 @@
+# gulp validate
+# gulp package
+
 ./node_modules/.bin/ngc -p ngcConfig.json && \
   gulp bundle.es6 && \
-  find ./src -name "*metadata.json" | sed 's/\.\/src//g' | awk '{print "mv \.\/src" $1 " ./dist/esm" $1}' | sh
-  find ./src -name "*.d.ts" | sed 's/\.\/src//g' | awk '{print "mv \.\/src" $1 " ./dist/esm" $1}' | sh
+  find ./src -name "*metadata.json" | sed 's/\.\/src//g' | awk '{print "mv \.\/src" $1 " ./dist/esm" $1}' | sh && \
+  find ./src -name "*.d.ts" | sed 's/\.\/src//g' | awk '{print "mv \.\/src" $1 " ./dist/esm" $1}' | sh && \
+  git checkout src/components/slides/swiper-widget.d.ts
+
+# git status | grep "src/" | grep ".js" | awk '{print "rm " $1}' | sh
+# git status | grep "src/" | grep "ngfactory.ts" | awk '{print "rm " $1}'
+
