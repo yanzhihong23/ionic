0e73be7d13d9cbc334533a8f9c3182518a400a3e
Max Lynch <max@drifty.com>
2016-08-17 09:47:18 -0500

    Added comments

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -12,7 +12,17 @@ console.log('LocalForageDriver', CordovaSQLiteDriver);
 
 /**
  * Storage is an easy way to store key/value pairs and other complicated
- * data in a way that uses a variety of storage engines underneath.
+ * data in a way that uses a variety of storage engines underneath. Currently,
+ * Storage uses localforage underneath to abstract away the various storage
+ * engines while still providing a simple API.
+ *
+ * When running natively, Storage will prioritize using SQLite, as it's one of
+ * the most stable and widely used file-based databases, and avoids some of the
+ * pitfalls of things like localstorage that the OS can decide to clear out in
+ * low disk-space situations.
+ *
+ * When running in the web or as a Progressive Web App, Storage will attempt to use
+ * IndexedDB, WebSQL, and localstorage, in that order.
  */
 @Injectable()
 export class Storage {
@@ -39,18 +49,37 @@ export class Storage {
     ])
   }
 
+  /**
+   * Get the value assocated with the given key.
+   * @return Promise that resolves with the value
+   */
   get(key: string): Promise<any> {
     return this._db.getItem(key);
   }
 
+  /**
+   * Set the value for the given key.
+   * @param key the key to identify this value
+   * @param value the value for this key
+   * @return Promise that resolves when the value is set
+   */
   set(key: string, value: any) {
     return this._db.setItem(key, value);
   }
 
+  /**
+   * Remove any value associated with this key.
+   * @param key the key to identify this value
+   * @return Promise that resolves when the value is removed
+   */
   remove(key: string) {
     return this._db.removeItem(key);
   }
 
+  /**
+   * Clear the entire key value store. WARNING: HOT!
+   * @return Promise that resolves when the kv store is cleared
+   */
   clear() {
     return this._db.clear();
   }
@@ -69,41 +98,12 @@ export class Storage {
     return this._db.keys();
   }
 
+  /**
+   * Iterate through each key,value pair.
+   * @param iteratorCallback a callback of the form (value, key, iterationNumber)
+   */
   forEach(iteratorCallback: (value: any, key: string, iterationNumber: Number) => any) {
     return this._db.iterate(iteratorCallback);
   }
 
-  /*
-  getJson(key: string): Promise<any> {
-    return this.get(key).then(value => {
-      try {
-        return JSON.parse(value);
-      } catch (e) {
-        console.warn('Storage getJson(): unable to parse value for key', key, ' as JSON');
-        throw e; // rethrowing exception so it can be handled with .catch()
-      }
-    });
-  }
-
-  setJson(key: string, value: any): Promise<any> {
-    try {
-      return this.set(key, JSON.stringify(value));
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  }
-
-
-  remove(key: string) {
-    return this._strategy.remove(key);
-  }
-
-  query(query: string, params?: any) {
-    return this._strategy.query(query, params);
-  }
-
-  clear() {
-    return this._strategy.clear();
-  }
-  */
 }
